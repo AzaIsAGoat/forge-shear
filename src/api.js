4c18112da0f646bcda709e7bc2fe71db1a7e53a7
AzaIsAGoat <azania.ora.zwane@gmail.com>
2026-09-25 12:54:44 +0200

    Added api suffix base url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://forgeandshearapi-apg3gcazgvb4f8fc.uaenorth-01.azurewebsites.net', 
+  baseURL: 'https://forgeandshearapi-apg3gcazgvb4f8fc.uaenorth-01.azurewebsites.net/api', 
   headers: {
     'Content-Type': 'application/json'
   }
